327a3d5bdbb30765d82c757668f3c5d3f2ef243e
Kwon <a4220662@hotmail.com>
2024-06-15 04:49:04 +0800

    fix：patchGroup should not extends Defult.class

--- src/main/java/com/kwon/validatorplus/groups/PatchGroup.java
+++ src/main/java/com/kwon/validatorplus/groups/PatchGroup.java
@@ -1,10 +1,9 @@
 package com.kwon.validatorplus.groups;
 
-import javax.validation.groups.Default;
 
 /**
  * Patch Jakarta Bean Validation group.
  * @author Kwon
  */
-public interface PatchGroup extends Default {
+public interface PatchGroup  {
 }
